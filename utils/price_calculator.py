42390ce254b4c0825000f2aebfb7989ad20f376d
Sudhakar17 <sudhakarece17@gmail.com>
2020-05-19 19:03:41 +0200

    Combine the features

---
+++ utils/price_calculator.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np
+import joblib
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.room_ix = 3
+        self.bedrooms_ix = 4
+        self.population_ix = 5
+        self.household_ix = 6
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, x, y=None):
+        rooms_per_household = x[self.room_ix] / x[self.household_ix]
+        population_per_household = x[self.population_ix] / x[self.household_ix]
+        bedrooms_per_room = x[self.bedrooms_ix]/ x[self.room_ix]
+        new_features = np.array((rooms_per_household, population_per_household, bedrooms_per_room))
+        x = np.hstack((x, new_features))
+        return x
